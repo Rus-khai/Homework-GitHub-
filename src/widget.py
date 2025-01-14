3bb74c86da7a1bcefd05dcc9bcd6a6b4c21ee0c0
Ruslan Khairullin <ruselkotya@gmail.com>
2025-01-14 23:19:23 +0300

    add docstrings in wadget.py

--- src/widget.py
+++ src/widget.py
@@ -2,6 +2,9 @@ from masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(info_card: str) -> str:
+    """
+    Функция принимает и обрабатывать информацию как о картах, так и о счетах и маскирует её
+    """
     number_card = ''
     type_card = ''
     for char in info_card:
@@ -18,6 +21,12 @@ def mask_account_card(info_card: str) -> str:
 
 
 def get_date(date: str) -> str:
+    """
+    функцию , которая принимает на вход строку с датой в формате
+    "2024-03-11T02:26:18.671407"
+    и возвращает строку с датой в формате
+    "ДД.ММ.ГГГГ"("11.03.2024").
+    """
     filter_date = date[:10].split('-')
     reversed_filter_date = filter_date[::-1]
     str_reversed_filter_date = str(reversed_filter_date[0]
