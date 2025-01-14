ce99f6833cdb976baecc8e27c4e8c37a8a40bf40
Ruslan Khairullin <ruselkotya@gmail.com>
2025-01-14 21:16:53 +0300

    past homework

---
+++ src/masks.py
@@ -0,0 +1,26 @@
+import os
+
+
+def get_mask_card_number(card_number: [str, int]) -> str:
+    """ "
+    Функция get_mask_card_number принимает на вход номер карты и возвращает ее маску.
+    """
+    str_card_number = str(card_number)
+    return (
+        str_card_number[:4]
+        + " "
+        + str_card_number[4:6]
+        + "**"
+        + " "
+        + "****"
+        + " "
+        + str_card_number[-4:]
+    )
+
+
+def get_mask_account(account_number: [str, int]) -> str:
+    """
+    Функция get_mask_account принимает на вход номер счета и возвращает его маску.
+    """
+    str_account_number = str(account_number)
+    return "**" + str_account_number[-4:]
