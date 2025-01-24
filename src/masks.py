834b6957958fdcbffaf7466c449c8e0e667c9cb0
Ruslan Khairullin <ruselkotya@gmail.com>
2025-01-25 00:31:09 +0300

    add tests

--- src/masks.py
+++ src/masks.py
@@ -6,6 +6,8 @@ def get_mask_card_number(card_number: Union[str, int]) -> Union[str, int]:
     Функция get_mask_card_number принимает на вход номер карты и возвращает ее маску.
     """
     str_card_number = str(card_number)
+    if len(str_card_number) > 16 or len(str_card_number) < 16 or len(str_card_number) == 0:
+        raise ValueError("Не правильный номер карты")
     return str_card_number[:4] + " " + str_card_number[4:6] + "**" + " " + "****" + " " + str_card_number[-4:]
 
 
@@ -14,4 +16,8 @@ def get_mask_account(account_number: Union[str, int]) -> Union[str, int]:
     Функция get_mask_account принимает на вход номер счета и возвращает его маску.
     """
     str_account_number = str(account_number)
+    if len(str_account_number) > 20 or len(str_account_number) < 20 or len(str_account_number) == 0:
+        raise ValueError("Не правильный номер карты")
     return "**" + str_account_number[-4:]
+
+
