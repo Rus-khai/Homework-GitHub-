703de93c50c982b2fd206c68b5c5a05883edaf6a
Ruslan Khairullin <ruselkotya@gmail.com>
2025-02-21 10:17:01 +0300

    fixed database.py, README.md

--- src/database.py
+++ src/database.py
@@ -5,23 +5,27 @@ import pandas as pd
 
 def read_transaction_csv(base_date: Any):
     """Функция считывает csv.file и выводит список словарей с транзакциями"""
+    list_transaction_csv = []
     df = pd.read_csv(base_date, delimiter=';')
     transactions_csv = df.to_dict(orient='records')
     for transaction in transactions_csv:
-        return transaction
+        list_transaction_csv.append(transaction)
+    return list_transaction_csv
 
 
 base_date = 'data/transactions.csv'
-read_transaction_csv(base_date)
+print(read_transaction_csv(base_date))
 
 
 def read_transaction_excel(base_excel: Any):
     """Функция считывает excel.file и выводит список словарей с транзакциями"""
+    list_transaction_excel = []
     df = pd.read_excel(base_excel)
     transactions_excel = df.to_dict(orient='records')
     for transaction in transactions_excel:
-        return transaction
+        list_transaction_excel.append(transaction)
+    return list_transaction_excel
 
 
 base_excel = 'data/transactions_excel.xlsx'
-print(read_transaction_excel(base_excel))
+read_transaction_excel(base_excel)
