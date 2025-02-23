da38278955174b2ba264dffa24fff322f6f9f4e0
ruslankh <ruselkotya@gmail.com>
2025-02-23 08:39:32 +0300

    fixed tests test_database.py

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,30 +1,35 @@
+import unittest.mock
 
-from unittest.mock import patch
-
-import pandas as pd
-
-from src.database import read_transaction_csv
+from src.database import read_transaction_csv, read_transaction_excel
 
 
-@patch('pandas.read_csv')
+@unittest.mock.patch('pandas.read_csv')
 def test_database(get_mock):
-    mock_transaction = [{'id': 3330422.0, 'state': 'EXECUTED', 'date': '2023-08-05T07:11:26Z', 'amount': 30065.0, 'currency_name': 'Ruble', 'currency_code': 'RUB', 'from': 'Mastercard 9458117363112215', 'to': 'Visa 6335859532296628', 'description': 'Перевод с карты на карту'}, {'id': 3794942.0, 'state': 'EXECUTED', 'date': '2021-05-24T02:37:49Z', 'amount': 14174.0, 'currency_name': 'Yuan Renminbi', 'currency_code': 'CNY', 'from': 'Mastercard 8628645140673956', 'to': 'Счет 36990402090010935845', 'description': 'Перевод организации'}]
-    get_mock.return_value = mock_transaction
+    mock_transaction = [{'id': 3330422.0,
+                         'state': 'EXECUTED',
+                         'date': '2023-08-05T07:11:26Z',
+                         'amount': 30065.0,
+                         'currency_name': 'Ruble',
+                         'currency_code': 'RUB',
+                         'from': 'Mastercard 9458117363112215',
+                         'to': 'Visa 6335859532296628',
+                         'description': 'Перевод с карты на карту'}]
+
+    get_mock.return_value.to_dict.return_value = mock_transaction
     assert read_transaction_csv('data/transactions.csv') == mock_transaction
 
 
-# @patch('src.database.read_transaction_csv')
-# def test_read_transaction_excel(mock_get):
-#     mock_transactions = {'id': 650703.0,
-#                          'state': 'EXECUTED',
-#                          'date': '2023-09-05T11:30:32Z',
-#                          'amount': 16210.0,
-#                          'currency_name': 'Sol',
-#                          'currency_code': 'PEN',
-#                          'from': 'Счет 58803664561298323391',
-#                          'to': 'Счет 39745660563456619397',
-#                          'description': 'Перевод организации'}
-#
-#     mock_get.return_value = mock_transactions
-#     assert read_transaction_excel(base_excel) == mock_transactions
-#
+@unittest.mock.patch('pandas.read_excel')
+def test_read_transaction_excel(mock_get):
+    mock_transactions = [{'id': 3330422.0,
+                          'state': 'EXECUTED',
+                          'date': '2023-08-05T07:11:26Z',
+                          'amount': 30065.0,
+                          'currency_name': 'Ruble',
+                          'currency_code': 'RUB',
+                          'from': 'Mastercard 9458117363112215',
+                          'to': 'Visa 6335859532296628',
+                          'description': 'Перевод с карты на карту'}]
+
+    mock_get.return_value.to_dict.return_value = mock_transactions
+    assert read_transaction_excel('data/transactions_excel.xlsx') == mock_transactions
