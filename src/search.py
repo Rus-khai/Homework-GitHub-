7e5776d8567a904ccd588b7e1f517d6fd04a4f2f
ruslankh <ruselkotya@gmail.com>
2025-03-01 19:10:03 +0300

    add main.py

--- src/search.py
+++ src/search.py
@@ -1,5 +1,6 @@
 import re
 from database import read_transaction_csv
+from collections import Counter
 
 
 def search_description(list_transaction_dict, str_search):
@@ -7,11 +8,11 @@ def search_description(list_transaction_dict, str_search):
     pattern = re.compile(str_search)
     try:
         for transaction in list_transaction_dict:
-            result = re.search(pattern, transaction.get('description', ''))
+            result = re.search(pattern, transaction.get('description', 'Перевод с карты на карту'))
             if result:
                 result_list_transaction.append(transaction)
     except Exception as e:
-        result = f'Ошибка {e}'
+        result_list_transaction = f'Ошибка {e}'
     finally:
         if result_list_transaction:
             return result_list_transaction
@@ -19,6 +20,20 @@ def search_description(list_transaction_dict, str_search):
 
 
 
+# if __name__ == '__main__':
+#     base_date = 'data/transactions.csv'
+#     print(search_description(read_transaction_csv(base_date), 'счет'))
+
+def quantity_transactions(list_transaction_dict, categories):
+    descriptions = [transaction['description'] for transaction in list_transaction_dict]
+    counter = Counter(descriptions)
+    result_dict = {categories: counter[categories]}
+    return result_dict
+
+
+
+
+
 if __name__ == '__main__':
     base_date = 'data/transactions.csv'
-    print(search_description(read_transaction_csv(base_date), 'cчет'))
+    print(quantity_transactions(read_transaction_csv(base_date), 'Открытие вклада'))
